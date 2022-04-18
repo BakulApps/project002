49c3b3ba2138221f9c3dfed147f829a53324a99f
marifmuntaha <marifmuntaha@gmail.com>
2022-04-18 21:54:19 +0700

    180420222154

--- database/migrations/2021_03_07_141227_create_entity__student_table.php
+++ database/migrations/2021_03_07_141227_create_entity__student_table.php
@@ -13,107 +13,107 @@ class CreateEntityStudentTable extends Migration
      */
     public function up()
     {
-        Schema::create('student_entity__schools_from', function (Blueprint $table) {
-            $table->id('from_id');
-            $table->string('from_name', 50);
-            $table->string('from_category', 10);
-        });
-
-        Schema::create('student_entity__schools_kind', function (Blueprint $table) {
-            $table->id('kind_id');
-            $table->string('kind_name', 50);
-            $table->string('kind_category', 10);
-        });
-
-        Schema::create('student_entity__contests_field', function (Blueprint $table) {
-            $table->id('field_id');
-            $table->string('field_name', 20);
-        });
-
-        Schema::create('student_entity__contests_category', function (Blueprint $table) {
-            $table->id('category_id');
-            $table->string('category_name', 20);
-        });
-
-        Schema::create('student_entity__contests_achievement', function (Blueprint $table) {
-            $table->id('achievement_id');
-            $table->string('achievement_name', 50);
-        });
-
-        Schema::create('student_entity__contests_level', function (Blueprint $table) {
-            $table->id('level_id');
-            $table->string('level_name', 50);
-        });
-
-        Schema::create('student_entity__purposes', function (Blueprint $table) {
-            $table->id('purpose_id');
-            $table->string('purpose_name', 20);
-        });
-
-        Schema::create('student_entity__hobbies', function (Blueprint $table) {
-            $table->id('hobby_id');
-            $table->string('hobby_name', 20);
-        });
-
-        Schema::create('student_entity__scholarships', function (Blueprint $table) {
-            $table->id('scholarship_id');
-            $table->string('scholarship_name', 20);
-        });
-
-        Schema::create('student_entity__residences', function (Blueprint $table) {
-            $table->id('residence_id');
-            $table->string('residence_name', 50);
-        });
-
-        Schema::create('student_entity__status', function (Blueprint $table) {
-            $table->id('status_id');
-            $table->string('status_name', 20);
-        });
-
-        Schema::create('student_entity__status_active', function (Blueprint $table) {
-            $table->id('active_id');
-            $table->string('active_name', 20);
-        });
-
-        Schema::create('student_entity__status_out', function (Blueprint $table) {
-            $table->id('out_id');
-            $table->string('out_name', 20);
-        });
-
-        Schema::create('student_entity__levels', function (Blueprint $table) {
-            $table->id('level_id');
-            $table->string('level_name', 20);
-        });
-
-        Schema::create('student_entity__programs', function (Blueprint $table) {
-            $table->id('program_id');
-            $table->string('program_name', 50);
-        });
-
-        Schema::create('student_entity__parents_status', function (Blueprint $table) {
-            $table->id('status_id');
-            $table->string('status_name', 50);
-        });
-
-        Schema::create('student_entity__settings', function (Blueprint $table) {
-            $table->id('setting_id');
-            $table->string('setting_name',50);
-            $table->string('setting_value')->nullable();
-        });
-
-        Schema::create('student_entity__users', function (Blueprint $table) {
-            $table->id('user_id');
-            $table->string('user_name');
-            $table->string('user_pass');
-            $table->string('user_fullname');
-            $table->rememberToken();
-        });
-
-        Schema::create('student_entity__classes', function (Blueprint $table){
-            $table->id('student_class_id');
-            $table->string('student_id')->nullable();
-            $table->string('class_id')->nullable();
-        });
+//        Schema::create('student_entity__schools_from', function (Blueprint $table) {
+//            $table->id('from_id');
+//            $table->string('from_name', 50);
+//            $table->string('from_category', 10);
+//        });
+//
+//        Schema::create('student_entity__schools_kind', function (Blueprint $table) {
+//            $table->id('kind_id');
+//            $table->string('kind_name', 50);
+//            $table->string('kind_category', 10);
+//        });
+//
+//        Schema::create('student_entity__contests_field', function (Blueprint $table) {
+//            $table->id('field_id');
+//            $table->string('field_name', 20);
+//        });
+//
+//        Schema::create('student_entity__contests_category', function (Blueprint $table) {
+//            $table->id('category_id');
+//            $table->string('category_name', 20);
+//        });
+//
+//        Schema::create('student_entity__contests_achievement', function (Blueprint $table) {
+//            $table->id('achievement_id');
+//            $table->string('achievement_name', 50);
+//        });
+//
+//        Schema::create('student_entity__contests_level', function (Blueprint $table) {
+//            $table->id('level_id');
+//            $table->string('level_name', 50);
+//        });
+//
+//        Schema::create('student_entity__purposes', function (Blueprint $table) {
+//            $table->id('purpose_id');
+//            $table->string('purpose_name', 20);
+//        });
+//
+//        Schema::create('student_entity__hobbies', function (Blueprint $table) {
+//            $table->id('hobby_id');
+//            $table->string('hobby_name', 20);
+//        });
+//
+//        Schema::create('student_entity__scholarships', function (Blueprint $table) {
+//            $table->id('scholarship_id');
+//            $table->string('scholarship_name', 20);
+//        });
+//
+//        Schema::create('student_entity__residences', function (Blueprint $table) {
+//            $table->id('residence_id');
+//            $table->string('residence_name', 50);
+//        });
+//
+//        Schema::create('student_entity__status', function (Blueprint $table) {
+//            $table->id('status_id');
+//            $table->string('status_name', 20);
+//        });
+//
+//        Schema::create('student_entity__status_active', function (Blueprint $table) {
+//            $table->id('active_id');
+//            $table->string('active_name', 20);
+//        });
+//
+//        Schema::create('student_entity__status_out', function (Blueprint $table) {
+//            $table->id('out_id');
+//            $table->string('out_name', 20);
+//        });
+//
+//        Schema::create('student_entity__levels', function (Blueprint $table) {
+//            $table->id('level_id');
+//            $table->string('level_name', 20);
+//        });
+//
+//        Schema::create('student_entity__programs', function (Blueprint $table) {
+//            $table->id('program_id');
+//            $table->string('program_name', 50);
+//        });
+//
+//        Schema::create('student_entity__parents_status', function (Blueprint $table) {
+//            $table->id('status_id');
+//            $table->string('status_name', 50);
+//        });
+//
+//        Schema::create('student_entity__settings', function (Blueprint $table) {
+//            $table->id('setting_id');
+//            $table->string('setting_name',50);
+//            $table->string('setting_value')->nullable();
+//        });
+//
+//        Schema::create('student_entity__users', function (Blueprint $table) {
+//            $table->id('user_id');
+//            $table->string('user_name');
+//            $table->string('user_pass');
+//            $table->string('user_fullname');
+//            $table->rememberToken();
+//        });
+//
+//        Schema::create('student_entity__classes', function (Blueprint $table){
+//            $table->id('student_class_id');
+//            $table->string('student_id')->nullable();
+//            $table->string('class_id')->nullable();
+//        });
     }
 
     /**
@@ -123,25 +123,25 @@ class CreateEntityStudentTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('student_entity__schools_from');
-        Schema::dropIfExists('student_entity__schools_kind');
-        Schema::dropIfExists('student_entity__contests_field');
-        Schema::dropIfExists('student_entity__contests_category');
-        Schema::dropIfExists('student_entity__contests_achievement');
-        Schema::dropIfExists('student_entity__contests_level');
-        Schema::dropIfExists('student_entity__purposes');
-        Schema::dropIfExists('student_entity__hobbies');
-        Schema::dropIfExists('student_entity__scholarships');
-        Schema::dropIfExists('student_entity__scholarships_source');
-        Schema::dropIfExists('student_entity__residences');
-        Schema::dropIfExists('student_entity__status');
-        Schema::dropIfExists('student_entity__status_active');
-        Schema::dropIfExists('student_entity__status_out');
-        Schema::dropIfExists('student_entity__levels');
-        Schema::dropIfExists('student_entity__programs');
-        Schema::dropIfExists('student_entity__parents_status');
-        Schema::dropIfExists('student_entity__settings');
-        Schema::dropIfExists('student_entity__users');
-        Schema::dropIfExists('student_entity__classes');
+//        Schema::dropIfExists('student_entity__schools_from');
+//        Schema::dropIfExists('student_entity__schools_kind');
+//        Schema::dropIfExists('student_entity__contests_field');
+//        Schema::dropIfExists('student_entity__contests_category');
+//        Schema::dropIfExists('student_entity__contests_achievement');
+//        Schema::dropIfExists('student_entity__contests_level');
+//        Schema::dropIfExists('student_entity__purposes');
+//        Schema::dropIfExists('student_entity__hobbies');
+//        Schema::dropIfExists('student_entity__scholarships');
+//        Schema::dropIfExists('student_entity__scholarships_source');
+//        Schema::dropIfExists('student_entity__residences');
+//        Schema::dropIfExists('student_entity__status');
+//        Schema::dropIfExists('student_entity__status_active');
+//        Schema::dropIfExists('student_entity__status_out');
+//        Schema::dropIfExists('student_entity__levels');
+//        Schema::dropIfExists('student_entity__programs');
+//        Schema::dropIfExists('student_entity__parents_status');
+//        Schema::dropIfExists('student_entity__settings');
+//        Schema::dropIfExists('student_entity__users');
+//        Schema::dropIfExists('student_entity__classes');
     }
 }
--- database/seeders/StudentTableSeeder.php
+++ database/seeders/StudentTableSeeder.php
@@ -16,241 +16,241 @@ class StudentTableSeeder extends Seeder
     public function run()
     {
 
-        User::factory(2)->create();
-
-        $school_from = [
-            [1, 'MA', 'MA'],
-            [2,	'SMA',	'MA'],
-            [3,	'SMK',	'MA'],
-            [4,	'Paket C',	'MA'],
-            [6,	'SMA di Luar Negeri',	'MA'],
-            [7,	'MTs',	'MTS'],
-            [8,	'SMP',	'MTS'],
-            [9,	'MI',	'MI'],
-            [10, 'SD',	'MI'],
-            [11, 'SDLB', 'MI'],
-            [12, 'SD di Luar Negeri', 'MI'],
-            [13, 'Lainnya', 00],
-            [14, 'RA', 'RA'],
-            [15, 'TK', 'RA'],
-            [16, 'PAUD', 'RA'],
-            [17, 'Langsung dari Orang Tua', 'RA'],
-            [18, 'Kelompok Bermain', 'RA'],
-            [19, 'Paket B', 'MA'],
-            [20, 'Pondok Pesantren', 'MA'],
-            [21, 'SMP di Luar Negeri', 'MA'],
-        ];
-        for ($i=0;$i<count($school_from);$i++){
-            DB::table('student_entity__schools_from')->insert(
-                [
-                    'from_id' => $school_from[$i][0],
-                    'from_name' => $school_from[$i][1],
-                    'from_category' => $school_from[$i][2]
-                ]
-            );
-        }
-
-        $school_kind = [
-            [1, 'MA', 'ma'],
-            [2, 'SMA', 'ma'],
-            [3, 'SMK', 'ma'],
-            [4, 'Paket C', 'ma'],
-            [5, 'Pondok Pesantren', 'ma'],
-            [6, 'SMA di Luar Negeri', 'ma'],
-            [0, '--', 'ma'],
-            [11, 'MTS', 'mts'],
-            [12, 'SMP', 'mts'],
-            [13, 'Paket B', 'mts'],
-            [14, 'Pondok Pesantren', 'mts'],
-            [15, 'SMP di Luar Negeri', 'mts'],
-            [10, '--', 'mts'],
-            [20, '--', 'mi'],
-            [21, 'MI', 'mi'],
-            [25, 'SD di Luar Negeri', 'mi'],
-            [24, 'Pondok Pesantren', 'mi'],
-            [23, 'Paket A', 'mi'],
-            [22, 'SD', 'mi'],
-            [30, '--', 'ra'],
-            [31, 'TK', 'ra'],
-            [32, 'RA', 'ra'],
-            [33, 'PAUD', 'ra']
-        ];
-        for ($i=0;$i<count($school_kind);$i++){
-            DB::table('student_entity__schools_kind')->insert(
-                [
-                    'kind_id' => $school_kind[$i][0],
-                    'kind_name' => $school_kind[$i][1],
-                    'kind_category' => $school_kind[$i][2]
-                ]
-            );
-        }
-
-        $contest_field = [
-            [1, 'Akademik'],
-            [2, 'Keagamaan'],
-            [3, 'Teknologi'],
-            [4, 'Olahraga'],
-            [5, 'Pramuka/Paskibraka'],
-            [6, 'Karya Ilmiah'],
-            [7, 'Kesenian'],
-            [8, 'Pidato Bahasa Asing'],
-            [9, 'Lainnya'],
-            [10, 'Sains'],
-        ];
-        for ($i=0;$i<count($contest_field);$i++){
-            DB::table('student_entity__contests_field')->insert(
-                [
-                    'field_id' => $contest_field[$i][0],
-                    'field_name' => $contest_field[$i][1],
-                ]
-            );
-        }
-
-        $contest_category = [
-            [1, 'Perorangan'],
-            [2, 'Kelompok']
-        ];
-        for ($i=0;$i<count($contest_category);$i++){
-            DB::table('student_entity__contests_category')->insert(
-                [
-                    'category_id' => $contest_category[$i][0],
-                    'category_name' => $contest_category[$i][1],
-                ]
-            );
-        }
-
-        $contest_achievement = [
-            [1, 'Tidak Meraih Juara'],
-            [2, 'Juara I/Medali Emas'],
-            [3, 'Juara II/Medali Perak'],
-            [4, 'Juara III/Medali Perunggu'],
-            [5, 'Juara Harapan I'],
-            [6, 'Juara Harapan II'],
-            [7, 'Juara Harapan III'],
-            [8, 'Juara Favorit']
-        ];
-        for ($i=0;$i<count($contest_achievement);$i++){
-            DB::table('student_entity__contests_achievement')->insert(
-                [
-                    'achievement_id' => $contest_achievement[$i][0],
-                    'achievement_name' => $contest_achievement[$i][1],
-                ]
-            );
-        }
-
-        $contest_level = [
-            [1, 'Tingkat Kabupaten/Kota'],
-            [2, 'Tingkat Provinsi'],
-            [3, 'Tingkat Nasional'],
-            [4, 'Tingkat Internasional']
-        ];
-        for ($i=0;$i<count($contest_level);$i++){
-            DB::table('student_entity__contests_level')->insert(
-                [
-                    'level_id' => $contest_level[$i][0],
-                    'level_name' => $contest_level[$i][1],
-                ]
-            );
-        }
-
-        $hobby = [
-            [1, 'Olahraga'],
-            [2, 'Kesenian'],
-            [3, 'Membaca'],
-            [4, 'Menulis'],
-            [5, 'Jalan-Jalan'],
-            [6, 'Lainnya']
-        ];
-        for ($i=0;$i<count($hobby);$i++){
-            DB::table('student_entity__hobbies')->insert(
-                [
-                    'hobby_id' => $hobby[$i][0],
-                    'hobby_name' => $hobby[$i][1],
-                ]
-            );
-        }
-
-        $purpose = [
-            [1, 'Lainnya'],
-            [2, 'PNS'],
-            [3, 'TNI/Polri'],
-            [4, 'Guru/Dosen'],
-            [5, 'Dokter'],
-            [6, 'Politikus'],
-            [7, 'Wiraswasta'],
-            [8, 'Seniman/Artis'],
-            [9, 'Ilmuwan'],
-            [10, 'Agamawan']
-        ];
-        for ($i=0;$i<count($purpose);$i++){
-            DB::table('student_entity__purposes')->insert(
-                [
-                    'purpose_id' => $purpose[$i][0],
-                    'purpose_name' => $purpose[$i][1]
-                ]
-            );
-        }
-
-        $residence = [
-            [1, 'Tinggal dengan Orangtua/Wali'],
-            [2, 'Ikut Saudara/Kerabat'],
-            [3, 'Asrama Madrasah'],
-            [4, 'Kontrak/Kost'],
-            [5, 'Asrama Pesantren'],
-            [6, 'Panti Asuhan'],
-            [7, 'Rumah Singgah'],
-            [8, 'Lainnya']
-        ];
-        for ($i=0;$i<count($residence);$i++){
-            DB::table('student_entity__residences')->insert(
-                [
-                    'residence_id' => $residence[$i][0],
-                    'residence_name' => $residence[$i][1]
-                ]
-            );
-        }
-
-        $program = [
-            [1, 'Unggulan Tahfidz'],
-            [2, 'Unggulan Kitab'],
-            [3, 'Unggulan Sains & Bahasa'],
-            [4, 'Kelas Reguler']
-        ];
-        for ($i=0;$i<count($program);$i++){
-            DB::table('student_entity__programs')->insert(
-                [
-                    'program_id' => $program[$i][0],
-                    'program_name' => $program[$i][1]
-                ]
-            );
-        }
-
-        $status = [
-            [1,	'Masih Hidup'],
-            [2,	'Sudah Meninggal'],
-            [3,	'Tidak Diketahui'],
-        ];
-        for ($i=0;$i<count($status);$i++){
-            DB::table('student_entity__parents_status')->insert(
-                [
-                    'status_id' => $status[$i][0],
-                    'status_name' => $status[$i][1]
-                ]
-            );
-        }
-
-        $setting = [
-            ['app_name', 'Sistem Informasi Madrasah Terpadu'],
-            ['app_alias', 'SIMADU'],
-            ['app_logo', ''],
-            ['app_desc', ''],
-
-        ];
-        for ($i=0;$i<count($setting);$i++){
-            DB::table('student_entity__settings')->insert([
-                'setting_name' => $setting[$i][0],
-                'setting_value' => $setting[$i][1]
-            ]);
-        }
+//        User::factory(2)->create();
+//
+//        $school_from = [
+//            [1, 'MA', 'MA'],
+//            [2,	'SMA',	'MA'],
+//            [3,	'SMK',	'MA'],
+//            [4,	'Paket C',	'MA'],
+//            [6,	'SMA di Luar Negeri',	'MA'],
+//            [7,	'MTs',	'MTS'],
+//            [8,	'SMP',	'MTS'],
+//            [9,	'MI',	'MI'],
+//            [10, 'SD',	'MI'],
+//            [11, 'SDLB', 'MI'],
+//            [12, 'SD di Luar Negeri', 'MI'],
+//            [13, 'Lainnya', 00],
+//            [14, 'RA', 'RA'],
+//            [15, 'TK', 'RA'],
+//            [16, 'PAUD', 'RA'],
+//            [17, 'Langsung dari Orang Tua', 'RA'],
+//            [18, 'Kelompok Bermain', 'RA'],
+//            [19, 'Paket B', 'MA'],
+//            [20, 'Pondok Pesantren', 'MA'],
+//            [21, 'SMP di Luar Negeri', 'MA'],
+//        ];
+//        for ($i=0;$i<count($school_from);$i++){
+//            DB::table('student_entity__schools_from')->insert(
+//                [
+//                    'from_id' => $school_from[$i][0],
+//                    'from_name' => $school_from[$i][1],
+//                    'from_category' => $school_from[$i][2]
+//                ]
+//            );
+//        }
+//
+//        $school_kind = [
+//            [1, 'MA', 'ma'],
+//            [2, 'SMA', 'ma'],
+//            [3, 'SMK', 'ma'],
+//            [4, 'Paket C', 'ma'],
+//            [5, 'Pondok Pesantren', 'ma'],
+//            [6, 'SMA di Luar Negeri', 'ma'],
+//            [0, '--', 'ma'],
+//            [11, 'MTS', 'mts'],
+//            [12, 'SMP', 'mts'],
+//            [13, 'Paket B', 'mts'],
+//            [14, 'Pondok Pesantren', 'mts'],
+//            [15, 'SMP di Luar Negeri', 'mts'],
+//            [10, '--', 'mts'],
+//            [20, '--', 'mi'],
+//            [21, 'MI', 'mi'],
+//            [25, 'SD di Luar Negeri', 'mi'],
+//            [24, 'Pondok Pesantren', 'mi'],
+//            [23, 'Paket A', 'mi'],
+//            [22, 'SD', 'mi'],
+//            [30, '--', 'ra'],
+//            [31, 'TK', 'ra'],
+//            [32, 'RA', 'ra'],
+//            [33, 'PAUD', 'ra']
+//        ];
+//        for ($i=0;$i<count($school_kind);$i++){
+//            DB::table('student_entity__schools_kind')->insert(
+//                [
+//                    'kind_id' => $school_kind[$i][0],
+//                    'kind_name' => $school_kind[$i][1],
+//                    'kind_category' => $school_kind[$i][2]
+//                ]
+//            );
+//        }
+//
+//        $contest_field = [
+//            [1, 'Akademik'],
+//            [2, 'Keagamaan'],
+//            [3, 'Teknologi'],
+//            [4, 'Olahraga'],
+//            [5, 'Pramuka/Paskibraka'],
+//            [6, 'Karya Ilmiah'],
+//            [7, 'Kesenian'],
+//            [8, 'Pidato Bahasa Asing'],
+//            [9, 'Lainnya'],
+//            [10, 'Sains'],
+//        ];
+//        for ($i=0;$i<count($contest_field);$i++){
+//            DB::table('student_entity__contests_field')->insert(
+//                [
+//                    'field_id' => $contest_field[$i][0],
+//                    'field_name' => $contest_field[$i][1],
+//                ]
+//            );
+//        }
+//
+//        $contest_category = [
+//            [1, 'Perorangan'],
+//            [2, 'Kelompok']
+//        ];
+//        for ($i=0;$i<count($contest_category);$i++){
+//            DB::table('student_entity__contests_category')->insert(
+//                [
+//                    'category_id' => $contest_category[$i][0],
+//                    'category_name' => $contest_category[$i][1],
+//                ]
+//            );
+//        }
+//
+//        $contest_achievement = [
+//            [1, 'Tidak Meraih Juara'],
+//            [2, 'Juara I/Medali Emas'],
+//            [3, 'Juara II/Medali Perak'],
+//            [4, 'Juara III/Medali Perunggu'],
+//            [5, 'Juara Harapan I'],
+//            [6, 'Juara Harapan II'],
+//            [7, 'Juara Harapan III'],
+//            [8, 'Juara Favorit']
+//        ];
+//        for ($i=0;$i<count($contest_achievement);$i++){
+//            DB::table('student_entity__contests_achievement')->insert(
+//                [
+//                    'achievement_id' => $contest_achievement[$i][0],
+//                    'achievement_name' => $contest_achievement[$i][1],
+//                ]
+//            );
+//        }
+//
+//        $contest_level = [
+//            [1, 'Tingkat Kabupaten/Kota'],
+//            [2, 'Tingkat Provinsi'],
+//            [3, 'Tingkat Nasional'],
+//            [4, 'Tingkat Internasional']
+//        ];
+//        for ($i=0;$i<count($contest_level);$i++){
+//            DB::table('student_entity__contests_level')->insert(
+//                [
+//                    'level_id' => $contest_level[$i][0],
+//                    'level_name' => $contest_level[$i][1],
+//                ]
+//            );
+//        }
+//
+//        $hobby = [
+//            [1, 'Olahraga'],
+//            [2, 'Kesenian'],
+//            [3, 'Membaca'],
+//            [4, 'Menulis'],
+//            [5, 'Jalan-Jalan'],
+//            [6, 'Lainnya']
+//        ];
+//        for ($i=0;$i<count($hobby);$i++){
+//            DB::table('student_entity__hobbies')->insert(
+//                [
+//                    'hobby_id' => $hobby[$i][0],
+//                    'hobby_name' => $hobby[$i][1],
+//                ]
+//            );
+//        }
+//
+//        $purpose = [
+//            [1, 'Lainnya'],
+//            [2, 'PNS'],
+//            [3, 'TNI/Polri'],
+//            [4, 'Guru/Dosen'],
+//            [5, 'Dokter'],
+//            [6, 'Politikus'],
+//            [7, 'Wiraswasta'],
+//            [8, 'Seniman/Artis'],
+//            [9, 'Ilmuwan'],
+//            [10, 'Agamawan']
+//        ];
+//        for ($i=0;$i<count($purpose);$i++){
+//            DB::table('student_entity__purposes')->insert(
+//                [
+//                    'purpose_id' => $purpose[$i][0],
+//                    'purpose_name' => $purpose[$i][1]
+//                ]
+//            );
+//        }
+//
+//        $residence = [
+//            [1, 'Tinggal dengan Orangtua/Wali'],
+//            [2, 'Ikut Saudara/Kerabat'],
+//            [3, 'Asrama Madrasah'],
+//            [4, 'Kontrak/Kost'],
+//            [5, 'Asrama Pesantren'],
+//            [6, 'Panti Asuhan'],
+//            [7, 'Rumah Singgah'],
+//            [8, 'Lainnya']
+//        ];
+//        for ($i=0;$i<count($residence);$i++){
+//            DB::table('student_entity__residences')->insert(
+//                [
+//                    'residence_id' => $residence[$i][0],
+//                    'residence_name' => $residence[$i][1]
+//                ]
+//            );
+//        }
+//
+//        $program = [
+//            [1, 'Unggulan Tahfidz'],
+//            [2, 'Unggulan Kitab'],
+//            [3, 'Unggulan Sains & Bahasa'],
+//            [4, 'Kelas Reguler']
+//        ];
+//        for ($i=0;$i<count($program);$i++){
+//            DB::table('student_entity__programs')->insert(
+//                [
+//                    'program_id' => $program[$i][0],
+//                    'program_name' => $program[$i][1]
+//                ]
+//            );
+//        }
+//
+//        $status = [
+//            [1,	'Masih Hidup'],
+//            [2,	'Sudah Meninggal'],
+//            [3,	'Tidak Diketahui'],
+//        ];
+//        for ($i=0;$i<count($status);$i++){
+//            DB::table('student_entity__parents_status')->insert(
+//                [
+//                    'status_id' => $status[$i][0],
+//                    'status_name' => $status[$i][1]
+//                ]
+//            );
+//        }
+//
+//        $setting = [
+//            ['app_name', 'Sistem Informasi Madrasah Terpadu'],
+//            ['app_alias', 'SIMADU'],
+//            ['app_logo', ''],
+//            ['app_desc', ''],
+//
+//        ];
+//        for ($i=0;$i<count($setting);$i++){
+//            DB::table('student_entity__settings')->insert([
+//                'setting_name' => $setting[$i][0],
+//                'setting_value' => $setting[$i][1]
+//            ]);
+//        }
     }
 }
